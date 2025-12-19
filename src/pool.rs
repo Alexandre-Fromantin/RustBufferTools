347516783f402526a7e01d869cf53392340095c9
Alexandre-Fromantin <oalexandrefromantin@gmail.com>
2025-12-19 21:23:56 +0100

    add: pool struct with new method

---
+++ src/pool.rs
@@ -0,0 +1,15 @@
+struct Pool<T> {
+    stack: Vec<T>,
+}
+
+impl<T: Default> Pool<T> {
+    pub fn new(capacity: usize) -> Self {
+        let mut stack = Vec::with_capacity(capacity);
+
+        for _ in 0..capacity {
+            stack.push(T::default());
+        }
+
+        Self { stack }
+    }
+}
