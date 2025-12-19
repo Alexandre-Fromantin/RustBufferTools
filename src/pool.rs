454a14f2a3294f7f8407e4edee1e772e9bde922f
Alexandre-Fromantin <oalexandrefromantin@gmail.com>
2025-12-19 22:58:09 +0100

    add: get() method of Pool struct, and fix Pool user-type

--- src/pool.rs
+++ src/pool.rs
@@ -1,22 +1,31 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 struct Pool<T> {
     stack: Vec<T>,
 }
 
 impl<T: Default> Pool<T> {
-    pub fn new(capacity: usize) -> Rc<Self> {
+    pub fn new(capacity: usize) -> Rc<RefCell<Self>> {
         let mut stack = Vec::with_capacity(capacity);
 
         for _ in 0..capacity {
             stack.push(T::default());
         }
 
-        Rc::new(Self { stack })
+        Rc::new(RefCell::new(Self { stack }))
+    }
+
+    pub fn get(pool: Rc<RefCell<Self>>) -> Option<PoolGuard<T>> {
+        let get_value = pool.borrow_mut().stack.pop()?;
+
+        Some(PoolGuard {
+            value: get_value,
+            pool: Rc::clone(&pool),
+        })
     }
 }
 
 struct PoolGuard<T> {
     value: T,
-    pool: Rc<Pool<T>>,
+    pool: Rc<RefCell<Pool<T>>>,
 }
